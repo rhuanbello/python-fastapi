49e7257ac0fd02689f3b4696f6783ca1e781f7ec
Rhuan Bello <rhuambello@gmail.com>
2025-01-02 15:57:40 -0300

    build: installing python on CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from testcontainers.postgres import PostgresContainer
+from testcontainers.postgres import PostgresContainer  # type: ignore
 
 from fast_zero.app import app
 from fast_zero.database import get_session
@@ -42,17 +42,17 @@ def client(session):
         yield client
 
 
-@pytest.fixture(scope='session') # Create a fixture to create a database engine
+@pytest.fixture(scope='session')  # Create a fixture to create a database engine
 def engine():
-    with PostgresContainer('postgres:16', driver='psycopg') as postgres: # Create a Postgres container
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:  # Create a Postgres container
         _engine = create_engine(postgres.get_connection_url())
 
-        with _engine.begin(): 
-            yield _engine # Return the engine
+        with _engine.begin():
+            yield _engine  # Return the engine
 
 
 @pytest.fixture  # create a fixture to with a memory database engine & returns a session
-def session(engine): # receives the engine fixture
+def session(engine):  # receives the engine fixture
     table_registry.metadata.create_all(engine)  # creates the database structure (tables) based on the metadata in table_registry
 
     with Session(engine) as session:  # start a session that is a temporary connection to the database / "with" stmt make the session closes
